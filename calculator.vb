eb03037c0946a223e383e635490a70d46b9d1a0c
A191138 <55289470+A191138@users.noreply.github.com>
2019-10-03 21:20:07 +0500

    Create calculator.vb

---
+++ calculator.vb
@@ -0,0 +1,36 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim op As Char
+        Dim answer As Double
+
+        Console.WriteLine("please enter the first number")
+        num1 = Console.ReadLine
+
+        Console.WriteLine("please enter an operator")
+        op = Console.ReadLine
+
+        Console.WriteLine("please enter the second number")
+        num2 = Console.ReadLine
+
+        If op = "+" Then
+            answer = num1 + num2
+        End If
+        If op = "-" Then
+            answer = num1 - num2
+        End If
+        If op = "*" Then
+            answer = num1 * num2
+        End If
+        If op = "/" Then
+            answer = num1 / num2
+        End If
+
+        Console.WriteLine("ANSWER = " & answer)
+
+        Console.ReadKey()
+    End Sub
+End Module
